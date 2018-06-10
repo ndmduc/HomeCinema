a28a47ab40dabec6a73a857c88039bac9b735bc4
Duc Nguyen <ducdmn@outlook.com>
2018-06-10 19:39:56 +0700

    add viewmodel

---
+++ HomeCinema/Infrastructure/Core/ApiControllerBase.cs
@@ -0,0 +1,65 @@
+﻿using HomeCinema.Data.Infrastructure;
+using HomeCinema.Data.Repositories;
+using HomeCinema.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace HomeCinema.Infrastructure.Core
+{
+    public class ApiControllerBase : ApiController
+    {
+        protected readonly IEntityBaseRepository<Error> errorRepo;
+
+        protected readonly IUnitOfWork unitOfWork;
+
+        public ApiControllerBase(IEntityBaseRepository<Error> errorRepository, IUnitOfWork unitofwork)
+        {
+            this.errorRepo = errorRepository;
+            this.unitOfWork = unitofwork;
+        }
+
+        protected HttpResponseMessage CreateHttpResponse(HttpRequestMessage request, Func<HttpResponseMessage> function)
+        {
+            HttpResponseMessage response = null;
+            try
+            {
+                response = function.Invoke();
+            }
+            catch (DbUpdateException ex)
+            {
+                LogError(ex);
+                response = request.CreateResponse(System.Net.HttpStatusCode.BadRequest, ex.InnerException.Message);
+            }
+            catch(Exception ex)
+            {
+                LogError(ex);
+                response = request.CreateResponse(System.Net.HttpStatusCode.InternalServerError, ex.InnerException.Message);
+            }
+
+            return response;
+        }
+
+        private void LogError(Exception ex)
+        {
+            try
+            {
+                Error error = new Error
+                {
+                    Message = ex.Message,
+                    StackTrace = ex.StackTrace,
+                    DateCreated = DateTime.Now
+                };
+
+                this.errorRepo.Add(error);
+                this.unitOfWork.Commit();
+            }
+            catch { }
+            
+        }
+    }
+}
---
+++ HomeCinema/Infrastructure/Validators/MovieViewModelValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using HomeCinema.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HomeCinema.Infrastructure.Validators
+{
+    public class MovieViewModelValidator : AbstractValidator<MovieViewModel>
+    {
+        public MovieViewModelValidator()
+        {
+            RuleFor(m => m.GenreId).GreaterThan(0).WithMessage("Select a Genre");
+            RuleFor(m => m.Director).NotEmpty().Length(1, 100).WithMessage("Select a Director");
+            RuleFor(m => m.Writer).NotEmpty().Length(1, 50).WithMessage("Select a Writer");
+            RuleFor(m => m.Producer).NotEmpty().Length(1, 50).WithMessage("Select a Producer");
+            RuleFor(m => m.Description).NotEmpty().WithMessage("Select a Description");
+            RuleFor(m => m.Rating).InclusiveBetween((byte)0, (byte)5).WithMessage("Rating must be less than or equal to 5");
+            RuleFor(m => m.TrailerURI).NotEmpty().Must(ValidTrailerUri).WithMessage("Only Youtube trailers are supported");
+        }
+
+        private bool ValidTrailerUri(string trailerUri)
+        {
+            return (!string.IsNullOrEmpty(trailerUri) && trailerUri.ToLower().StartsWith("https://www.youtube.com/watch?"));
+        }
+    }
+}
